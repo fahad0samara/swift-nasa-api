e06954d42272617fbb6b925f2b1147c80de47fcf
fahad0samarar <fah8ad@outlook.com>
2024-02-16 21:00:52 +0200

    add

---
+++ nasa/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  nasa
+//
+//  Created by fahad samara on 2/16/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
