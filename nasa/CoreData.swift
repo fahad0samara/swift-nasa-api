45649151e7bd0ecae67e2391272bc462b9d2294d
fahad0samarar <fah8ad@outlook.com>
2024-02-19 20:31:34 +0200

    add cpre data

---
+++ nasa/CoreData.swift
@@ -0,0 +1,18 @@
+//
+//  CoreData.swift
+//  nasa
+//
+//  Created by fahad samara on 2/19/24.
+//
+
+import SwiftUI
+
+struct CoreData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreData()
+}
